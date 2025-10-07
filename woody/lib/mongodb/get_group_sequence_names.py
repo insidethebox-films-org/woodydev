0dba943fe8ce897184899f8e1b4e2aa2de9866cf
BuildTools <unconfigured@null.spigotmc.org>
2025-10-07 17:10:26 +1300

    changed it so the items sort in the asset browser

--- woody/lib/mongodb/get_group_sequence_names.py
+++ woody/lib/mongodb/get_group_sequence_names.py
@@ -6,23 +6,21 @@ db = DB_instance(woody.projectName)
 db = db.connect
 
 def get_group_sequence_names(groupType):
-    
     if groupType == "Assets Group":
         collection = db["groups"]
     else:
         collection = db["sequences"]
     
-    return [doc["name"] for doc in collection.find({}, {"name": 1})]
+    cursor = collection.find({}, {"name": 1}).sort("name", 1)
+    return [doc["name"] for doc in cursor]
 
 def get_elements_names(group_name):
-
     if db["groups"].find_one({"name": group_name}):
         collection = db["assets"]
         query = {"group": group_name}
     else:
         collection = db["shots"]
         query = {"sequence": group_name}
-        
-    return [doc["name"] for doc in collection.find(query)]
-
     
+    cursor = collection.find(query).sort("name", 1)
+    return [doc["name"] for doc in cursor]
