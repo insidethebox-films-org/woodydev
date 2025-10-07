ac222339338b6d76bab0df025fa073b2345cce0d
BuildTools <unconfigured@null.spigotmc.org>
2025-10-07 16:57:32 +1300

    Basic version of asset browser

--- woody/lib/mongodb/get_group_sequence_names.py
+++ woody/lib/mongodb/get_group_sequence_names.py
@@ -1,16 +1,28 @@
 from ...tool import WoodyInstance
 from ...database.db_instance import DB_instance
 
+woody = WoodyInstance()
+db = DB_instance(woody.projectName)
+db = db.connect
+
 def get_group_sequence_names(groupType):
-    
-    woody = WoodyInstance()
-    db = DB_instance(woody.projectName)
-    db = db.connect
     
     if groupType == "Assets Group":
         collection = db["groups"]
     else:
         collection = db["sequences"]
     
-    
     return [doc["name"] for doc in collection.find({}, {"name": 1})]
+
+def get_elements_names(group_name):
+
+    if db["groups"].find_one({"name": group_name}):
+        collection = db["assets"]
+        query = {"group": group_name}
+    else:
+        collection = db["shots"]
+        query = {"sequence": group_name}
+        
+    return [doc["name"] for doc in collection.find(query)]
+
+    
--- woody/ui/frames/asset_browser.py
+++ woody/ui/frames/asset_browser.py
@@ -1,4 +1,5 @@
 from ..widgets import CTkListbox
+from ...lib.mongodb.get_group_sequence_names import get_group_sequence_names, get_elements_names
 
 import customtkinter as ctk
 
@@ -16,23 +17,57 @@ class AssetBrowserFrame:
             border_color="#5a5a5a",
             )
         self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(1, weight=5)
+        self.frame.grid_columnconfigure(2, weight=5)
+        
+        self.frame.grid_rowconfigure(0, weight=1)
+        
+    def on_root_select(self, selected):
+        self.current_group_type = selected
+        self.group_list_box.delete(0, "END")
+        groups = get_group_sequence_names(selected)
+        
+        for i, group in enumerate(groups):
+            self.group_list_box.insert(i, group)
+        
+        self.element_list_box.delete(0, "END")
+            
+    def on_group_select(self, selected):
+        self.element_list_box.delete(0, "END")
+        elements = get_elements_names(selected)
+        
+        for i, element in enumerate(elements):
+            self.element_list_box.insert(i, element)
     
     def create_widgets(self):
-    
-        listbox = CTkListbox(
+        
+        # Root list box
+        self.root_list_box = CTkListbox(
+            self.frame,
+            highlight_color="#86753d",
+            hover_color="#5a5a5a",
+            border_width=2,
+            command=self.on_root_select
+            )
+        self.root_list_box.grid(row=0, column=0, sticky="nsew", pady=5, padx=(5,0))
+        self.root_list_box.insert(0, "Assets Group") 
+        self.root_list_box.insert("END", "Shots Group")
+        
+        # Group list box
+        self.group_list_box = CTkListbox(
+            self.frame,
+            highlight_color="#86753d",
+            hover_color="#5a5a5a",
+            border_width=2,
+            command=self.on_group_select
+        )
+        self.group_list_box.grid(row=0, column=1, sticky="nsew", pady=5, padx=(5,0))
+        
+        # Element list box
+        self.element_list_box = CTkListbox(
             self.frame,
             highlight_color="#86753d",
             hover_color="#5a5a5a",
             border_width=2
-            )
-        listbox.pack(fill="both", expand=True, padx=5, pady=5)
-
-        listbox.insert(0, "Option 0")
-        listbox.insert(1, "Option 1")
-        listbox.insert(2, "Option 2")
-        listbox.insert(3, "Option 3")
-        listbox.insert(4, "Option 4")
-        listbox.insert(5, "Option 5")
-        listbox.insert(6, "Option 6")
-        listbox.insert(7, "Option 7")
-        listbox.insert("END", "Option 8")
+        )
+        self.element_list_box.grid(row=0, column=2, sticky="nsew", pady=5, padx=5)
