c398c000a926cc107f3bbf4bfb37aad696e94a8d
Oscar Bartle <Oscar.f.bartle@gmail.com>
2025-11-26 12:43:53 +1300

    Added DCC exe intergration

--- woody/dcc/blender/blender.py
+++ woody/dcc/blender/blender.py
@@ -1,24 +1,21 @@
 from ...objects.dcc import DCC
-from ...objects.woody import Woody
-from ...ui.dcc.dcc_gui import DccGui
-import subprocess
+from ...objects.directory import Directory
 
+import subprocess
 import os
 
 @DCC.register("blender")
 class Blender(DCC):
     
     def open_file(self):
-        executable = Woody().blenderExecutable + "blender.exe"
+        executable = Directory().get_dcc_executable("blender")
         socket = os.path.join(os.path.dirname(__file__), "blender_socket.py")
         addon = os.path.join(os.path.dirname(__file__), "addon","__init__.py")
         
         subprocess.Popen([
             executable,
             "--python", socket, "--python", addon
-        ]) 
-        
-        #DccGui()
-    
+        ])
+
         return True
-        
+         
